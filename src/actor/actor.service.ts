9349cbf90ee623e78b45acbc8212663d4999c328
avvakumovid <avvakumovid@gmail.com>
2022-11-24 16:21:16 +0300

    feat: aggregate for Actor

--- src/actor/actor.service.ts
+++ src/actor/actor.service.ts
@@ -34,7 +34,25 @@ export class ActorService {
                 ]
             }
         }
-        return await this.ActorModel.find(options).select('-updatedAt -__v').sort({ createdAt: 'desc' }).exec()
+
+
+
+        return await this.ActorModel.aggregate().match(options).lookup({
+            from: 'Movie',
+            localField: '_id',
+            foreignField: 'actors',
+            as: 'movies'
+        }).addFields({
+            countMovies: {
+                $size: '$movies'
+            }
+        })
+            .project({
+                __v: 0,
+                updatedAt: 0,
+                movies: 0 
+            })
+            .sort({ createdAt: -1 }).exec()
     }
 
 
